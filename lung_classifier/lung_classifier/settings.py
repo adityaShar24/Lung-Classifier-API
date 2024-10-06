bc4516f0b38e758839f95efdf8221f32db92c624
Aditya <aditya@fikaa.in>
2024-10-06 18:58:51 +0530

    added: token auth

--- lung_classifier/lung_classifier/settings.py
+++ lung_classifier/lung_classifier/settings.py
@@ -133,4 +133,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+
 from lung_classifier.local_settings import *
--- lung_classifier/lung_classifier/urls.py
+++ lung_classifier/lung_classifier/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('classify/', include('classifier.urls')),
+    path('auth/', include('custom_auth.urls')),
 ]
