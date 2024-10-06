f61b34a31428785c1c46493dcea3e89807ddacc7
Aditya <aditya@fikaa.in>
2024-10-06 15:19:20 +0530

    added: User Views

--- lung_classifier/custom_auth/views.py
+++ lung_classifier/custom_auth/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import (
+    CreateAPIView,
+    GenericAPIView,
+)
+from .serializers import (
+    UserRegistrationSerializer,
+    UserLoginSerializer,
+    )
 
-# Create your views here.
+class UserRegisterView(CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    
+    def create(self , request , *args , **kwargs):
+        serializer = self.get_serializer(data= request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.save()
+        return Response(
+            {'message': 'User has been registered successfully.' , 'user': serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+
+class UserLoginView(GenericAPIView):
+    serializer_class = UserLoginSerializer
+    
+    def post(self , request, *args , **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(rasie_exception = True)
+        user = serializer.validated_data['user']
+        
+        return Response(
+            {
+                "message": "User has been logged in successfully.",
+                "email": user.email,
+                "role": user.role,
+            },
+            status=status.HTTP_200_OK
+        )
