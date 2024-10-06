b21b2e013eba3e9fdbbe87e94aebc26941005131
Aditya <aditya@fikaa.in>
2024-10-06 15:08:15 +0530

    added: serializers

---
+++ lung_classifier/custom_auth/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import authenticate
+from rest_framework.serializers import (
+    ModelSerializer,
+    CharField,
+    EmailField,
+    Serializer,
+    ValidationError
+)
+
+from .models import User
+
+class UserRegistrationSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'role']  
+        extra_kwargs = {'password': {'write_only': True}} 
+        
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            role=validated_data['role'] 
+        )
+        user.set_password(validated_data['password']) 
+        user.save()
+        return user
+    
+class UserLoginSerializer(Serializer):
+    email = EmailField(required=True)
+    password = CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(email=email, password=password)
+        if user is None:
+            raise ValidationError("Invalid email or password.")
+        
+        attrs['user'] = user
+        return attrs
+
+    def to_representation(self, instance):
+        """Customize the representation of the validated user data."""
+        return {
+            'email': instance['user'].email,
+            'role': instance['user'].role  # Include role in the response
+        }
