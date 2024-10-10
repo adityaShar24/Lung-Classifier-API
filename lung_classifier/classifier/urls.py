43124a3b229103609a15e1e9cc5e0331b7304f06
Aditya <aditya@fikaa.in>
2024-10-10 11:54:57 +0530

    changes in views

--- lung_classifier/classifier/urls.py
+++ lung_classifier/classifier/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import (
     LungCancerClassifierView,
     GetAllClassifiedImages,
@@ -11,4 +13,4 @@ urlpatterns = [
     path('get-classified-images/', GetAllClassifiedImages.as_view(), name='classified-images'),
     path('delete-classified-image/<int:pk>/', DeleteClassifiedImageView.as_view(), name='delete-classified-image'),
     path('detail-classified-image/<int:pk>/', GetClassifiedImageDetail.as_view(), name='detail-classified-image'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- lung_classifier/classifier/views.py
+++ lung_classifier/classifier/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
-from django.conf import settings
-from django.http import JsonResponse
-from keras.models import load_model
-import numpy as np
+import os
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from django.conf import settings
+from keras.models import load_model
+from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView , DestroyAPIView , RetrieveAPIView
 from rest_framework.parsers import MultiPartParser ,FormParser
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import ImageClassifierSerializer
 from classifier.models import Image
+from rest_framework.generics import ListAPIView , DestroyAPIView , RetrieveAPIView
+from sklearn.metrics import confusion_matrix, roc_curve, auc, precision_recall_curve
+import seaborn as sns
 
 ML_MODEL_PATH = settings.MODEL_PATH
 
@@ -37,17 +41,105 @@ class LungCancerClassifierView(APIView):
             
             image_instance = Image(user=user, image=file_obj, prediction=prediction["prediction"])
             image_instance.save()
+            generate_graph = self._generate_graphs(prediction)
+            graph_urls = {
+                "confusion_matrix": os.path.join(settings.MEDIA_URL, f"plots/{self.request.user.username}/confusion_matrix.png"),
+                "roc_curve": os.path.join(settings.MEDIA_URL, f"plots/{self.request.user.username}/roc_curve.png"),
+                "precision_recall_curve": os.path.join(settings.MEDIA_URL, f"plots/{self.request.user.username}/precision_recall_curve.png"),
+            }
             
             serializer = ImageClassifierSerializer(image_instance)              
 
             return Response({
                 "message": prediction["message"],
-                "image_data": serializer.data
+                "image_data": serializer.data,
+                "graphs": graph_urls
             }, status=status.HTTP_200_OK)
         
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def _generate_graphs(self, prediction):
+        """Generates the required graphs after making a prediction."""
+        # Create a unique directory for each user to store their plots
+        user_folder = os.path.join(settings.MEDIA_ROOT, f'plots/{self.request.user.username}')
+        os.makedirs(user_folder, exist_ok=True)
+        
+        predicted_class = prediction['prediction']
+        
+        y_true = [0] 
+        y_pred = [0]  
 
+        # Generate Confusion Matrix and save it to the user's folder
+        self._plot_confusion_matrix(y_true, y_pred, classes=['lung_aca', 'lung_n', 'lung_scc'], user_folder=user_folder)
+        self._plot_roc_curve(y_true, y_pred, user_folder=user_folder)
+
+        # Generate Precision-Recall Curve
+        self._plot_precision_recall_curve(y_true, y_pred, user_folder=user_folder)
+
+    def _plot_confusion_matrix(self, y_true, y_pred, classes, user_folder):
+        """Plots and saves the confusion matrix in the user folder."""
+        cm = confusion_matrix(y_true, y_pred, labels=[0, 1, 2])
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
+        plt.title('Confusion Matrix')
+        plt.ylabel('True Class')
+        plt.xlabel('Predicted Class')
+        
+        # Save confusion matrix to user's folder
+        plot_path = os.path.join(user_folder, 'confusion_matrix.png')
+        plt.savefig(plot_path)
+        plt.close()
+        
+    def _plot_confusion_matrix(self, y_true, y_pred, classes, user_folder):
+        """Plots and saves the confusion matrix."""
+        cm = confusion_matrix(y_true, y_pred, labels=[0, 1, 2])
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
+        plt.title('Confusion Matrix')
+        plt.ylabel('True Class')
+        plt.xlabel('Predicted Class')
+        
+        # Save confusion matrix
+        plot_path = os.path.join(user_folder, 'confusion_matrix.png')
+        plt.savefig(plot_path)
+        plt.close()
+
+    def _plot_roc_curve(self, y_true, y_pred, user_folder):
+        """Plots and saves the ROC curve."""
+        fpr, tpr, _ = roc_curve(y_true, y_pred)
+        roc_auc = auc(fpr, tpr)
+        
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+        
+        # Save ROC curve
+        plot_path = os.path.join(user_folder, 'roc_curve.png')
+        plt.savefig(plot_path)
+        plt.close()
+
+    def _plot_precision_recall_curve(self, y_true, y_pred, user_folder):
+        """Plots and saves the precision-recall curve."""
+        precision, recall, _ = precision_recall_curve(y_true, y_pred)
+        
+        plt.figure()
+        plt.plot(recall, precision, lw=2, color='blue')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('Precision-Recall Curve')
+        
+        # Save precision-recall curve
+        plot_path = os.path.join(user_folder, 'precision_recall_curve.png')
+        plt.savefig(plot_path)
+        plt.close()
+        
     def _read_image(self, file_obj):
         """Converts the uploaded file to an OpenCV image and preprocesses it."""
         image_data = np.fromstring(file_obj.read(), np.uint8)
